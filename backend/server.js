7e873f71ecea4ffae59b29f68157429ed2267080
M. Jasvanth <mjasvanth85@gmail.com>
2026-05-11 19:39:56 +0530

    forgot

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 // Initialize Express app
 const app = express();
@@ -11,6 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files (images, css, js)
+app.use(express.static(path.join(__dirname, '../')));
 
 // =====================
 // ROUTES
